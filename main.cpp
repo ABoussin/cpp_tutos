fc11d3cb56a30f6ebc9c6b6d37c3111c9b3bef27
Ebaksis <toky@aboussin.fr>
2017-04-20 18:31:33 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,22 +7,23 @@
 using namespace std;
 
 struct Attribute {
-    string name;
-    string value;
+	string name;
+	string value;
 };
 
 struct Node {
-    vector<Attribute> attr;
-    vector<Node*> children;
+	string name;
+	vector<Attribute> attr;
+	vector<Node*> children;
 };
 
 void addAttribute(Node * node, string name, string value)
 {
-    Attribute attr;
-    attr.name = name;
-    attr.value = value;
+	Attribute attr;
+	attr.name = name;
+	attr.value = value;
 
-    node->attr.push_back(attr);
+	node->attr.push_back(attr);
 }
 
 void addNode(Node * node, Node * child)
@@ -30,45 +31,105 @@ void addNode(Node * node, Node * child)
 	node->children.push_back(child);
 }
 
-void constructTree(Node * parent)
+void buildTree(Node* parent, char* nextLine, int* nbLine)
+{
+	Node* result = new Node();
+	stringstream lineS(nextLine);
+	string tagName;
+	char escapeChar;
+
+	(*nbLine)--;
+
+	lineS >> escapeChar >> tagName;
+	result->name = tagName;
+
+	string attrName;
+	while (lineS >> attrName)
+	{
+		string value;
+
+		lineS >> escapeChar; // get =
+		lineS >> escapeChar; // get "
+		lineS >> value;
+		value.pop_back(); // remove last "
+
+		cout << attrName << " " << value << "\n";
+
+		addAttribute(result, attrName, value);
+	}
+
+	addNode(parent, result);
+
+	char input[500];
+
+	while (*nbLine > 0)
+	{
+		cin.getline(input, 500, '>');
+		if (input[2] == '/')
+		{
+			return;
+		}
+		else
+		{
+			buildTree(result, input, nbLine);
+		}
+	}
+}
+
+void readTree(Node* node)
+{
+	cout << node->name << " : \n";
+
+	for (vector<Attribute>::iterator it = node->attr.begin(); it != node->attr.end(); ++it)
+	{
+		cout <<"\t"<< (*it).name << " -> " << (*it).value << "\n";
+	}
+
+	if (node->children.size() != 0)
+	{
+		for (vector<Node*>::iterator it = node->children.begin(); it != node->children.end(); ++it)
+		{
+			readTree(*it);
+		}
+	}
+}
+
+string getQuerieResult(vector<char*> querie, Node * root)
 {
 
+	for (vector<char*>::iterator it = querie.begin(); it != querie.end(); ++it)
+	{
+		
+	}
 }
 
 int main() {
-    int n, q;
-    cin>>n>>q;
-    
-    Node root;
-       
-    
-    for(int i = 0; i<n; i++)
-    {
-        char input[500];
-        cin.getline(input, 500, '>');
-        
-        stringstream lineS(input);
-        
-        string name;
-        char c;
-        
-        lineS>>c>>name;
-        
-        string attrName;
-        cout<<name<<"\n";
-        while(lineS>>attrName)
-        {
-            char c;
-            string value;
-            lineS>>c; // get =
-            lineS>>c; // get "
-            lineS>>value;
-            value.pop_back(); // remove last "
-            cout<<attrName<<" "<<value<<"\n";
-        }
-        
-        cout<<"\n";
-    }
-    
-    return 0;
+	int n, q;
+	Node root;
+	char inputLine[500];
+	
+	// Get parameters
+	cin >> n >> q;
+	// Get first line
+	cin.getline(inputLine, 500, '>');
+	// Build tree
+	buildTree(&root, inputLine, &n);
+
+	// Get queries
+	for (int i = q; i > 0; --i)
+	{
+		char* p;
+		cin >> inputLine;
+		vector<char*> querie;
+		for (p = strtok(inputLine, "."); p; p = strtok(NULL, "."))
+		{
+			querie.push_back(p);
+			//printf("\"%s\"\n", p);
+		}
+
+		getQuerieResult();
+	}
+	readTree(&root);
+	
+	return 0;
 }
